325f3be2db9a4da6f7384d1a81635ab0511e7cfe
Vitaly Gubarev <vitaly1090@mail.ru>
2025-06-12 16:13:48 +1000

    init

---
+++ src/test/java/org/example/eventmanager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
